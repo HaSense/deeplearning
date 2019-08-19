9f7e37e6f037ff8cf5b6afc69cdff3a8d77822d3
Virtual Sense <virtual_sense@naver.com>
2019-08-19 10:29:22 +0900

    Update step_function.py

--- step_function.py
+++ step_function.py
@@ -6,6 +6,7 @@
 퍼셉트론에서는 활성화 함수로 계단함수를 이용합니다.
 '''
 import numpy as np
+import matplotlib.pylab as plt
 
 def intuitive_setp_function(x):
     if x>0 :
@@ -17,6 +18,9 @@ def step_function(x):
     y = x > 0
     return y.astype(np.int)
 
+def step_function2(x):
+    return np.array(x>0, dtype=np.int)
+
 #setep function test
 x = np.array([-1.0, 1.0, 2.0])
 print(x)
@@ -24,3 +28,10 @@ y = x > 0
 print(y)
 y = y.astype(np.int)
 print(y)
+
+#그림을 그려봅시다
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function2(x)
+plt.plot(x,y)
+plt.ylim(-0.1, 1.1)
+plt.show()
