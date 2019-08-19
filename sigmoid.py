69c1573800246a565afa72a023fa083ee3c6e1f1
Virtual Sense <virtual_sense@naver.com>
2019-08-19 10:48:44 +0900

    시그모이드&렐루

--- sigmoid.py
+++ sigmoid.py
@@ -4,6 +4,9 @@ import matplotlib.pylab as plt
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def relu(x):
+    return np.maximum(0,x)
+
 x = np.array([-1.0, 1.0, 2.0])
 result = sigmoid(x)
 print(result)
@@ -18,4 +21,10 @@ y = sigmoid(x)
 
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
+plt.show()
+
+y = relu(x)
+plt.plot(x, y)
+plt.xlim(-5.0, 5.0)
+plt.ylim(-1.0, 5.0)
 plt.show()
